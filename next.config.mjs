4cdcc76cbd8facc3bf6b592b0bbb44d9f331d217
Vaibhav7689 <rtxninja7689@gmail.com>
2025-09-30 14:45:10 +0530

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-nextjs' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-nextjs' : ''
+  assetPrefix:  '/portfolio-nextjs/',
+  basePath: '/portfolio-nextjs'
 }
 
 export default nextConfig
